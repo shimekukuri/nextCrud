53d2470ad418a1a5c59f1c6f200a6725e42ec54f
Tyler Hutchinson <jhutchinson192390@gmail.com>
2022-07-25 01:09:47 -0400

    finished product

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let express = require("express");
 let bodyParser = require("body-parser");
 const { response } = require("express");
+const db = require("./config");
 
 const app = express();
 const port = 3200;
@@ -15,6 +16,12 @@ app.use(
 app.get(`/`, (request, response) => {
   return response.json({ info: `Node.js, epxress, and postgresql API` });
 });
+
+app.get("/users", db.getUsers);
+app.get("/users/:id", db.getUsersById);
+app.post("/users/", db.createUser);
+app.put("users/:id", db.updateUser);
+app.delete("/users/:id", db.deleteUser);
 
 app.listen(port, () => {
   console.log(`listening on port number ${port}`);
